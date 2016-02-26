978a78952b35750e9988da8d9b2f76240a4f49df
ismail <emcpalnd@gmail.com>
2016-02-26 02:13:40 +0200

    ddddf

--- app/src/main/java/app/memory_games_app/home.java
+++ app/src/main/java/app/memory_games_app/home.java
@@ -1,7 +1,9 @@
 package app.memory_games_app;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class home extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/AGA-Cordoba-V2-Bold.ttf");
+
+
+
+        TextView tx = (TextView)findViewById(R.id.welcome_txt);
+        tx.setTypeface(custom_font);
+        TextView numbers_bt_text = (TextView)findViewById(R.id.numbers_bt_text);
+        numbers_bt_text.setTypeface(custom_font);
+        TextView images_bt_text = (TextView)findViewById(R.id.images_bt_text);
+        images_bt_text.setTypeface(custom_font);
+
     }
+
+
+
+
 }
---
+++ app/src/main/java/app/memory_games_app/libs/view.java
@@ -0,0 +1,14 @@
+package app.memory_games_app.libs;
+
+import android.graphics.Typeface;
+import android.widget.TextView;
+
+/**
+ * Created by ismail on 2/26/2016.
+ */
+public class view {
+
+
+
+
+}
