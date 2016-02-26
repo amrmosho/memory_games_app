2b5e3b0c84ccbae352f42dbe376d4926df0282cc
ismail <emcpalnd@gmail.com>
2016-02-26 20:43:28 +0200

    ddddf

---
+++ app/src/main/java/app/memory_games_app/order_images.java
@@ -0,0 +1,13 @@
+package app.memory_games_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class order_images extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_images);
+    }
+}
